36530153f28ad996bb33694de56c9da3a3968573
Pouet Forever <pouet.forever@gmail.com>
2017-02-07 21:38:52 +0100

    fix d02
    Signed-off-by: Pouet Forever <pouet.forever@gmail.com>

--- d01/ex02/ackermann.ml
+++ d01/ex02/ackermann.ml
@@ -1,5 +1,5 @@
 let rec ackermann m n =
-    if m == 0 then n + 1
+    if m = 0 then n + 1
     else if m > 0 && n == 0 then ackermann (m - 1) 1
     else if m > 0 && n > 0 then begin
         let tmp = ackermann m (n - 1) in
