72b504f19f4494c07548eb032da80367f3dbf205
pouet_forever <pouet.forever@gmail.com>
2017-04-10 18:56:54 +0200

    fix euclidian distance + ex07
    Signed-off-by: pouet_forever <pouet.forever@gmail.com>

--- d05/ex05/eu_dist.ml
+++ d05/ex05/eu_dist.ml
@@ -2,7 +2,7 @@ let eu_dist a b =
     let x = ref 0. in
     for i = 0 to Array.length a - 1 do
         let tmp = a.(i) -. b.(i) in
-        x := tmp *. tmp
+        x := !x +. tmp *. tmp
     done;
     !x ** 0.5
 
--- d05/ex07/one_nn.ml
+++ d05/ex07/one_nn.ml
@@ -4,59 +4,47 @@ let eu_dist a b =
     let x = ref 0. in
     for i = 0 to Array.length a - 1 do
         let tmp = a.(i) -. b.(i) in
-        x := tmp *. tmp
+        x := !x +. tmp *. tmp
     done;
     !x ** 0.5
 
-let get_line f : radar =
-    let l = input_line f in
-    let s = Str.split (Str.regexp ",") l in
-    let s = List.rev s in
-    let c = List.hd s in
-    if String.length c <> 1 then failwith ("Invalid character : " ^ c);
-    let s = List.rev (List.tl s) in
-    let s = List.map (fun s -> if String.length s = 0 then failwith ("Invalid number : " ^ s); float_of_string s) s in
-    let tab = Array.of_list s in
-    (tab, c)
-
-let examples_of_file f =
-    let in_file = open_in f in
-    let ret = ref [] in
-    let get_input () =
-        try
-            while true do
-                let l = get_line in_file in
-                ret := l :: !ret
-            done
-        with
-        | End_of_file   -> close_in in_file
-        | Failure err   -> close_in in_file; ret := []; print_endline err
-        | _             -> ret := []; print_endline "Invalid file format"
-    in
-    get_input ();
-    List.rev !ret
-
-let one_nn (lst : radar list) (base : radar) =
+let one_nn (lst : radar list) ((base_tab, base_s as base) : radar) =
     let dist = ref infinity
     in
     let fct (tab, s) (tab', s') =
-        let eu = eu_dist tab tab' in
-        if String.compare s s' = 0 && eu < !dist then begin
+        let eu = eu_dist base_tab tab' in
+        if eu < !dist then begin
             dist := eu;
-            (tab, s)
+            (tab', s')
         end
-        else (tab', s')
+        else (tab, s)
     in
     let _, s = List.fold_left fct base lst in
     s
 
 let _ =
-(*
-    let f = "examples_of_file.ml" in
-    let f = "ionosphere.test.csv" in
-*)
-    let f = "ionosphere.train.csv" in
-    let r = examples_of_file f in
-    match r with
-    | hd :: tl  -> print_endline (one_nn tl hd)
-    | _         -> ()
+        let lst = [
+                ([| 1.; |], "toto");
+                ([| 20.; |], "tata");
+                ([| 300.; |], "titi");
+                ([| 4000.; |], "tutu");
+        ]
+        in
+        print_endline (one_nn lst ([| 5.; |], "hello"));
+        print_endline (one_nn lst ([| 30.; |], "hello"));
+        print_endline (one_nn lst ([| 500.; |], "hello"));
+        print_endline (one_nn lst ([| 6000.; |], "hello"));
+
+        print_endline "--------------";
+
+        let a = [
+                ([| 1.; 2.; 3.; 4.; |], "toto");
+                ([| 2.; 3.; 4.; 5.; |], "tata");
+                ([| 3.; 4.; 5.; 6.; |], "titi");
+                ([| 4.; 5.; 6.; 7.; |], "tutu");
+        ]
+        in
+        print_endline (one_nn a ([| -2.; -3.; -4.; -5.; |], "hello"));
+        print_endline (one_nn a ([| 2.; 3.; 4.; 5.; |], "hello"));
+        print_endline (one_nn a ([| 3.; 4.; 5.; 6.; |], "hello"));
+        print_endline (one_nn a ([| 4.; 5.; 6.; 7.; |], "hello"))
