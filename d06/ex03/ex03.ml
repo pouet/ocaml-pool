2e54f399b55cd3acef07e002d985a613eba4361a
pouet <nchrupal@student.42.fr>
2017-04-11 14:53:57 +0200

    d06/ex03
    Signed-off-by: pouet <nchrupal@student.42.fr>

--- d06/ex03/ex03.ml
+++ d06/ex03/ex03.ml
@@ -65,10 +65,10 @@ module Make : MAKEFIXED =
 
             let add x y = x + y
             let sub x y = x - y
-            let mul x y = int_of_float ((to_float x) *. (to_float y))
+            let mul x y = of_float ((to_float x) *. (to_float y))
             let div x y =
                 if y = 0 then 0
-                else int_of_float ((to_float x) /. (to_float y))
+                else of_float ((to_float x) /. (to_float y))
 
             let foreach x y f =
                 let rec loopup i =
@@ -95,5 +95,31 @@ let () =
     let y8 = Fixed8.of_float 21.32 in
     let r8 = Fixed8.add x8 y8 in
     print_endline (Fixed8.to_string r8);
-    Fixed4.foreach (Fixed4.zero) (Fixed4.one) (fun f -> print_endline
-    (Fixed4.to_string f))
+    Fixed4.foreach (Fixed4.zero) (Fixed4.one) (fun f -> print_endline (Fixed4.to_string f));
+    
+    let pf = Printf.printf in
+
+    pf "of_float  : %f\n" (Fixed8.to_float x8);
+    pf "of_int    : %d\n" (Fixed8.to_int x8);
+
+    pf "zero      : %s\n" (Fixed8.to_string Fixed8.zero);
+    pf "one       : %s\n" (Fixed8.to_string Fixed8.one);
+
+    pf "pred      : %s\n" (Fixed8.to_string (Fixed8.pred Fixed8.one));
+    pf "succ      : %s\n" (Fixed8.to_string (Fixed8.succ Fixed8.one));
+
+    pf "min       : %s\n" (Fixed8.to_string (Fixed8.min Fixed8.zero Fixed8.one));
+    pf "max       : %s\n" (Fixed8.to_string (Fixed8.max Fixed8.zero Fixed8.one));
+
+    pf "gth       : %s\n" (if Fixed8.gth Fixed8.zero Fixed8.one then "true" else "false");
+    pf "lth       : %s\n" (if Fixed8.lth Fixed8.zero Fixed8.one then "true" else "false");
+    pf "gte       : %s\n" (if Fixed8.gte Fixed8.zero Fixed8.one then "true" else "false");
+    pf "lte       : %s\n" (if Fixed8.lte Fixed8.zero Fixed8.one then "true" else "false");
+
+    pf "eqp       : %s\n" (if Fixed8.eqp Fixed8.zero Fixed8.zero then "true" else "false");
+    pf "eqs       : %s\n" (if Fixed8.eqs Fixed8.zero Fixed8.zero then "true" else "false");
+
+    pf "add       : %s\n" (Fixed8.to_string (Fixed8.add x8 x8));
+    pf "sub       : %s\n" (Fixed8.to_string (Fixed8.sub x8 x8));
+    pf "mul       : %s\n" (Fixed8.to_string (Fixed8.mul x8 x8));
+    pf "div       : %s\n" (Fixed8.to_string (Fixed8.div x8 x8));
